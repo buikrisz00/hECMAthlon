c485fc85a92a543fba340fc1c3a94da03a12427e
Krisztian Bui <buikrisz00@gmail.com>
2022-03-01 18:35:09 +0100

    Variable declarations changed

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,10 +1,10 @@
 const hECMAthlon = {
 
     getMaxValue: function (input) {
-        var maxValues = input.split(",");
-        var maxValue = 0;
-        for(var i = 0; i < maxValues.length; i++){
-            var tempValue = parseInt(maxValues[i]);
+        const maxValues = input.split(",");
+        let maxValue = 0;
+        for(let i = 0; i < maxValues.length; i++){
+            const tempValue = parseInt(maxValues[i]);
             if (tempValue > maxValue) {
                 maxValue = tempValue;
             }
@@ -13,9 +13,9 @@ const hECMAthlon = {
     },
 
     getGreaterThan: function (input) {
-        var greaterValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        var greaterNums = [];
-        for(var j = 0; j < greaterValues.length; j++) {
+        const greaterValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let greaterNums = [];
+        for(let j = 0; j < greaterValues.length; j++) {
             if (greaterValues[j] > parseInt(input)) {
                 greaterNums.push(greaterValues[j]);
             }
@@ -24,9 +24,9 @@ const hECMAthlon = {
     },
 
     fizzBuzz: function (input) {
-        var output = [];
-        for (var k = 1; k <= parseInt(input); k++) {
-            var value;
+        let output = [];
+        for (let k = 1; k <= parseInt(input); k++) {
+            let value;
             if(k % 5 === 0 && k % 3 === 0) value = "FizzBuzz";
             else if(k % 3 === 0) value = "Fizz";
             else if(k % 5 === 0) value = "Buzz";
