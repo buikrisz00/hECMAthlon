cc2fc51cca07cb8ebde0a6d6176c4a7156930123
Krisztian Bui <buikrisz00@gmail.com>
2022-03-01 18:35:58 +0100

    hecmathlon declared as a class

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,6 @@
-const hECMAthlon = {
+class hECMAthlon {
 
-    getMaxValue: function (input) {
+    getMaxValue(input) {
         const maxValues = input.split(",");
         let maxValue = 0;
         for(let i = 0; i < maxValues.length; i++){
@@ -10,9 +10,9 @@ const hECMAthlon = {
             }
         }
         return [maxValue];
-    },
+    }
 
-    getGreaterThan: function (input) {
+    getGreaterThan(input) {
         const greaterValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         let greaterNums = [];
         for(let j = 0; j < greaterValues.length; j++) {
@@ -21,9 +21,9 @@ const hECMAthlon = {
             }
         }
         return greaterNums;
-    },
+    }
 
-    fizzBuzz: function (input) {
+    fizzBuzz(input) {
         let output = [];
         for (let k = 1; k <= parseInt(input); k++) {
             let value;
