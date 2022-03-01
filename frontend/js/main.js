9c11700d25ba3a30f6489c7acb6cd23218556180
Krisztian Bui <buikrisz00@gmail.com>
2022-03-01 21:12:32 +0100

    fix done

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,15 +1,6 @@
 class hECMAthlonClass {
 
-    getMaxValue(input) {
-        /* const maxValues = input.split(",");
-        let maxValue = 0;
-        for(let i = 0; i < maxValues.length; i++){
-            const tempValue = parseInt(maxValues[i]);
-            if (tempValue > maxValue) {
-                maxValue = tempValue;
-            }
-        }
-        return [maxValue]; */
+    getMaxValue(input) { // In case of single input, it returns the result in separate rows 
         return input.split(",").reduce((previousNum, currentNum) => (parseInt(previousNum) > parseInt(currentNum)) ? [previousNum] : [currentNum])
     }
 
@@ -18,16 +9,6 @@ class hECMAthlonClass {
     }
 
     fizzBuzz(input) {
-        /* let output = [];
-        for (let k = 1; k <= parseInt(input); k++) {
-            let value;
-            if(k % 5 === 0 && k % 3 === 0) value = "FizzBuzz";
-            else if(k % 3 === 0) value = "Fizz";
-            else if(k % 5 === 0) value = "Buzz";
-            else value = k;
-            output.push(value);
-        }
-        return output; */
         return Array(parseInt(input)).fill(1).map((num, index) => {
             if ((index+1) % 5 === 0 && (index+1) % 3 === 0) {
                 return "FizzBuzz";
